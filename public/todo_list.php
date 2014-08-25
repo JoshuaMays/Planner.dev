dfe461918856d63455ad1f40c9169e100b7474ad
haydogsup <joshua.r.mays@gmail.com>
2014-08-25 17:04:34 -0500

    Added unordered list, and form for adding to list.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Todo List</title>
 </head>
 <body>
+	<h1>$_GET</h1>
+	<?php var_dump($_GET); ?>
+
+	<h1>$_POST</h1>
+	<?php var_dump($_POST); ?>
+
+	<h1>Todo List!</h1>
+
+	<ul>
+		<li>Make Sure Wearing Helmet.</li>
+		<li>Buckle Up.</li>
+		<li>Press Eject Button.</li>
+		<li>Hold On To Yer Butts!</li>
+	</ul>
+
+	<h2>Add a Task to the List</h2>
+	<form method="POST" action="/todo_list.php">
+		<input type="text" name="add_item" id="add_item" placeholder="fill me out, yo.">
+		
+		<input type="submit" value="Put me in, Coach">
+	</form>
 
 </body>
 </html>
