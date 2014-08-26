8e54895a2dc4db61e4d3baff54cf5d82ffcaed8d
haydogsup <joshua.r.mays@gmail.com>
2014-08-26 15:39:51 -0500

    oops, had to remove element style attributes

--- public/todo_list.php
+++ public/todo_list.php
@@ -2,30 +2,32 @@
 <html>
 <head>
 	<title>Todo List</title>
-	<link rel="stylesheet" href="/css...">
+	<link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-	<h1>$_GET</h1>
-	<?php var_dump($_GET); ?>
+	<div id="container">
+		<h1>$_GET</h1>
+		<?php var_dump($_GET); ?>
 
-	<h1>$_POST</h1>
-	<?php var_dump($_POST); ?>
+		<h1>$_POST</h1>
+		<?php var_dump($_POST); ?>
 
-	<h1>Todo List!</h1>
+		<h1 id="todo">Todo List!</h1>
 
-	<ul>
-		<li>Make Sure Wearing Helmet.</li>
-		<li>Buckle Up.</li>
-		<li>Press Eject Button.</li>
-		<li>Hold On To Yer Butts!</li>
-	</ul>
-
-	<h2>Add a Task to the List</h2>
-	<form method="POST" action="/todo_list.php">
-		<input type="text" name="add_item" id="add_item" placeholder="fill me out, yo." style="border-radius:7px;">
-		
-		<input type="submit" value="Put me in, Coach" style="border-radius:7px;">
-	</form>
-
+		<ul>
+			<li>Make Sure Wearing Helmet.</li>
+			<li>Buckle Up.</li>
+			<li>Press Eject Button.</li>
+			<li>Hold On To Yer Butts!</li>
+		</ul>
+		<div id="form">
+			<h2>Add a Task to the List</h2>
+			<form method="POST" action="/todo_list.php">
+				<input type="text" name="add_item" id="add_item" placeholder="fill me out, yo.">
+				
+				<input type="submit" value="Put me in, Coach">
+			</form>
+		</div>
+	</div>
 </body>
 </html>
