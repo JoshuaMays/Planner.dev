1f577e04a319fd1e8667cc675789fb55f2449f23
haydogsup <joshua.r.mays@gmail.com>
2014-09-11 16:59:23 -0500

    added class structure for address book, added import csv functionality

--- public/address_book.php
+++ public/address_book.php
@@ -1,42 +1,51 @@
 <?php
-
 // DEFINE A CONSTANT FILEPATH/NAME TO READ/WRITE
 define('FILENAME', '../data/address_book.csv');
 
-// INITIALIZE ADDRESS BOOK ARRAY VARIABLE
-$addressBook = [];
-
-// FUNCTION TO SAVE CSV ADDRESS ENTRIES TO CSV
-function saveCSVFile($addressBook, $filename = FILENAME) {
-    // Open file and overwrite contents.
-    $handle = fopen($filename, 'w');
-    // Loop through the entry and write to the csv file.
-    foreach ($addressBook as $row) {
-        fputcsv($handle, $row);
-    }
-    fclose($handle);
-}
-
-function readCSVFile($filename = FILENAME) {
-    // CREATE EMPTY $addressBook
-    $addressBook = [];
-    $handle = fopen($filename, 'r');
+// CLASS DEFINITION FOR ADDRESS DATA STORE
+class AddressDataStore {
+    public $filename = '';
     
-    while(!feof($handle)) {
-        $row = fgetcsv($handle);
-        if(!empty($row)) {
-            $addressBook[] = $row;
-        }
+    // CONSTRUCTOR FOR ADDRESS DATA STORE OBJECTS
+    public function __construct($filename) {
+        $this->filename = $filename;
     }
-    fclose($handle);
-    return $addressBook;
+
+    // FUNCTION TO READ CSV FILE DATA INTO AN ARRAY
+    public function readAddressBook() {
+        $addressBook = [];
+        $handle = fopen($this->filename, 'r');
+        while (!feof($handle)) {
+            $row = fgetcsv($handle);
+            if (!empty($row)) {
+                $addressBook[] = $row;
+            }
+        }
+        fclose($handle);
+        return $addressBook;
+    }
+
+    // FUNCTION TO SAVE ADDRESS ENTRY DATA TO CSV FILE
+    public function saveAddressBook($addressArray) {
+        $handle = fopen($this->filename, 'w');
+        
+        foreach($addressArray as $row) {
+            fputcsv($handle, $row);
+        }
+        fclose($handle);
+    }
 }
 
-// LOAD SAVED ADDRESS BOOK CSV TO POPULATE ADDRESS BOOK TABLE
-$addressBook = readCSVFile();
+// INSTANTIATE AN ADDRESS DATA STORE OBJECT
+$addressDS = new AddressDataStore(FILENAME);
 
-saveCSVFile($addressBook);
+// ASSIGN ADDRESS ARRAY TO A VARIABLE
+$addressBook = $addressDS->readAddressBook();
 
+$addressDS->readAddressBook($addressBook);
+
+// CAPTURE FORM DATA TO AN ADDRESS BOOK
+// if (!empty($_FILES)) 
 if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip']) && !empty($_POST['phone'])) {
     // PREVENT CODE INJECTION ON EACH INPUT
     foreach ($_POST as $key => $input) {
@@ -53,16 +62,40 @@ if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']
     // PUSH FORM ADDRESS BOOK ENTRY INTO ADDRESS BOOK ARRAY.
     $addressBook[] = $newEntry;
     // SAVE NEW ENTRY TO CSV FILE
-    saveCSVFile($addressBook);
+    $addressDS->saveAddressBook($addressBook);
 }
 
-// GET REQUEST TO REMOVE SINGLE ENTRIES FROM THE ADDRESS BOOK
+// ALLOW USER TO UPLOAD A CSV FILE TO IMPORT CONTACTS INTO THE ADDRESS BOOK
+if (count($_FILES) > 0 && $_FILES['fileUpload']['error'] == UPLOAD_ERR_OK) {
+        // UPLOAD DIRECTORY PATH
+        $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
+        $uploadFilename = basename($_FILES['fileUpload']['name']);
+        // UPLOADED PATH AND FILENAME
+        $savedFile = $uploadDir . $uploadFilename;
+        // MOVE TMP FILE TO UPLOADS DIRECTORY
+        move_uploaded_file($_FILES['fileUpload']['tmp_name'], $savedFile);
+}
+
+// AFTER FILE IS UPLOADED, SAVE NEW CONTACTS TO THE ADDRESS BOOK 
+if (!empty($_FILES) && $_FILES['fileUpload']['error'] == UPLOAD_ERR_OK) {
+    // CREATE A NEW ADDRESSDATASTORE OBJECT TO COPY THE NEW CONTACTS
+    $importedList = new AddressDataStore($savedFile);
+    $newlist = $importedList->readAddressBook();
+    // MERGE THE TWO CONTACT LISTS
+    $addressBook = array_merge($addressBook, $newlist);
+    $addressDS->saveAddressBook($addressBook);
+}
+
+//GET REQUEST TO REMOVE SINGLE ENTRIES FROM THE ADDRESS BOOK
+//CHANGE THIS TO A JS POST EVENT (HIDDEN FORM), NOT A GET REQUEST
 if (isset($_GET['remove'])) {
     $removeKey = $_GET['remove'];
     unset($addressBook[$removeKey]);
     $addressBook = array_values($addressBook);
-    saveCSVFile($addressBook);
+    $addressDS->saveAddressBook($addressBook);
 }
+
+
 
 ?>
 
@@ -76,50 +109,73 @@ if (isset($_GET['remove'])) {
 </head>
 <body>
     <div id="wrap">
-        <div id ="addressBook" class="row">
-            <div class="col-md-12 table-responsive">
-                <table class="table table-striped table-bordered table-condensed">
-                    <tr>
-                        <th>Delete</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Zip Code</th>
-                        <th>Phone Number</th>
-                    </tr>
-                    <!-- LOOPING THROUGH TOP LEVEL ARRAY OF ADDRESS BOOK ENTRIES -->
-                    <? foreach($addressBook as $entry => $row): ?>
-                        <tr><td><a href="?remove=<?=$entry?>">Delete</a></td>
-                            <!-- LOOPING THROUGH ARRAY OF CONTACT ENTRIES, PRINTING DATA INTO TABLE COLUMNS -->
-                            <? foreach($row as $columnData): ?>
-                                <td>
-                                    <?= $columnData ?>
-                                </td>
-                            <? endforeach; ?>
-                        </tr>
-                    <? endforeach; ?>
-                </table>
-            </div>
-        </div>
         <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <form method="POST" action="/address_book.php" class="form-horizontal">
+            <div id ="addressBook" class="row">
+                <div class="col-md-8 table-responsive">
+                    <h2>Contacts</h2>
+                    <table class="table table-striped table-bordered table-condensed">
+                        <tr>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>City</th>
+                            <th>State</th>
+                            <th>Zip Code</th>
+                            <th>Phone Number</th>
+                            <th>Delete</th>
+                        </tr>
+                        <!-- LOOPING THROUGH TOP LEVEL ARRAY OF ADDRESS BOOK ENTRIES -->
+                        <? foreach($addressBook as $entry => $row): ?>
+                            <tr>
+                                <!-- LOOPING THROUGH ARRAY OF CONTACT ENTRIES, PRINTING DATA INTO TABLE COLUMNS -->
+                                <? foreach($row as $columnData): ?>
+                                    <td>
+                                        <?= $columnData ?>
+                                    </td>
+                                <? endforeach; ?>
+                                <td><a href="?remove=<?=$entry?>">Delete</a></td>
+                            </tr>
+                        <? endforeach; ?>
+                    </table>
+                </div>
+                <div class="container">
+                    <div class="col-md-3">
                         <h2>Add a Contact</h2>
-                        <label for="name">Name: </label> <input type="text" name="name" id="name" class="form-control"> <br>
-                        <label for="address">Address: </label> <input type="text" name="address" id="address" class="form-control"> <br>
-                        <label for="city">City: </label> <input type="text" name="city" id="city" class="form-control"> <br>
-                        <label for="state">State: </label> <input type="text" name="state" id="state" class="form-control"> <br>
-                        <label for="zip">Zip: </label> <input type="text" name="zip" id="zip" class="form-control"> <br>
-                        <label for="phone">Phone: </label> <input type="tel" name="phone" id="phone" class="form-control"> <br>
-                        <input type="submit" value="Submit" class="btn btn-primary">
-                    </form>
+                        <form method="POST" action="/address_book.php" class="form-horizontal">
+                            <label for="name">Name: </label> 
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Name"> <br>
+                            <label for="address">Address: </label> 
+                            <input type="text" name="address" id="address" class="form-control" placeholder="Address"> <br>
+                            <label for="city">City: </label> 
+                            <input type="text" name="city" id="city" class="form-control" placeholder="City"> <br>
+                            <label for="state">State: </label> 
+                            <input type="text" name="state" id="state" class="form-control" placeholder="State"> <br>
+                            <label for="zip">Zip: </label> 
+                            <input type="text" name="zip" id="zip" class="form-control" placeholder="Zip Code"> <br>
+                            <label for="phone">Phone: </label> 
+                            <input type="tel" name="phone" id="phone" class="form-control" placeholder="Phone Number"> <br>
+                            <input type="submit" value="Submit" class="btn btn-primary">
+                        </form>
+                        <h2>Import Contacts</h2>
+                        <form method="POST" action="/address_book.php" enctype="multipart/form-data" class="form-horizontal">
+                            <label for="upload">File to Import:</label>
+                            <input type="file" name="fileUpload" id="fileUpload" class="form-control"><br>
+                            <input type="submit" value="Import" class="btn btn-primary">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <footer class="text-center">
+            &copy; 2014 &bull; Joshua Mays
+            </footer>
+        </div>
+    </div>
+    <script src="/js/jquery-1.11.0.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
 </body>
 </html>
